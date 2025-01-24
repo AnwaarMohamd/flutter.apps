69bb9c683c3bc3fd06f42440d5c7ea0a45d9917c
AnwaarMohamd <anwaarmohmd213@gmail.com>
2025-01-24 13:34:43 +0200

    features/onboarding

---
+++ adv_project/lib/core/routing/app_router.dart
@@ -0,0 +1,26 @@
+import 'package:adv_project/core/routing/routes.dart';
+import 'package:adv_project/features/onboarding/onboarding_screen.dart';
+import 'package:adv_project/features/ui/login_screen.dart';
+import 'package:flutter/material.dart';
+
+class AppRouter {
+  Route generateRoute(RouteSettings settings) {
+    switch (settings.name) {
+      case Routes.onBoardingScreen:
+        return MaterialPageRoute(
+          builder: (_) => const OnboardingScreen(),
+        );
+      case Routes.loginScreen:
+        return MaterialPageRoute(
+          builder: (_) => const LoginScreen(),
+        );
+      default:
+        return MaterialPageRoute(
+            builder: (_) => Scaffold(
+                  body: Center(
+                    child: Text('No route defined for ${settings.name}'),
+                  ),
+                ));
+    }
+  }
+}
